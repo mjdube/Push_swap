30c20f596d9890f1ad2594d872e212d8edf0af6a
Mlungisi DUBE <mdube@c4r6s11.wethinkcode.co.za>
2019-07-23 10:20:03 +0200

    23 July 2019

--- checker/createNode.c
+++ checker/createNode.c
@@ -6,18 +6,18 @@
 /*   By: mdube <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/07/20 11:57:20 by mdube             #+#    #+#             */
-/*   Updated: 2019/07/20 14:38:29 by mdube            ###   ########.fr       */
+/*   Updated: 2019/07/22 13:16:17 by mdube            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../libft/libft.h"
+#include "check.h"
 
 block		*createNode(void)
 {
 	block	*node;
 
-	node = (block*)ft_memalloc(block);
-	node->data = NULL;
+	node = (block*)ft_memalloc(sizeof(block));
+	node->data = 0;
 	node->next = NULL;
 	return (node);
 }
